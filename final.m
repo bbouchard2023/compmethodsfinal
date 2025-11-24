c76a198c1d16325252bad18c945523f51bedcdfe
bbouchard2023 <bbouchard2023@my.fit.edu>
2025-11-24 18:23:52 -0500

    Added clincian dialog for new pt data

--- final.m
+++ final.m
@@ -3,7 +3,7 @@
 %                                                             _,     ,_
 %  Brendan Bouchard, Joe Manfre, Panagiotis Vasileidis      .'/  ,_   \'.
 %  20251119                                                |  \__( >__/  |
-%  Last Modified: 20251119                                 \             /
+%  Last Modified: 20251124                                 \             /
 %                                                           '-..__ __..-'
 %                                                           jgs  /_\
 %==========================================================================
@@ -15,12 +15,17 @@
 %% Biomarker Evaluation
 % input pt biomarkers, evaluate good/bad
 
-
+height = input("Height (m)? ");
+weight = input("Weight (kg)? ");
+temperature = input("Body Temperature (°F)? ");
+sys_bp = input("Systolic Blood Pressure (mmHg)? ");
+dia_bp = input("Diastolic Blood Pressure (mmHg)? ");
+date = input("Today's Date (MMDDYYYY)? ");
 
 %% Data Storage
 % store pt biomarkers, generated data
 
-
+addVisitToExcel(filename, height, weight, temperature, sys_bp, dia_bp, date)
 
 %% Graphics
 % display pt data, trends, etc from view pt data
